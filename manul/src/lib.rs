d44c7ea57d04e5ff988b83fc7f8509f1b24ecb61
Bogdan Opanchuk <bogdan@opanchuk.net>
2024-09-29 11:25:02 -0700

    Add some clippy checks

--- manul/src/lib.rs
+++ manul/src/lib.rs
@@ -1,3 +1,13 @@
+#![warn(
+    clippy::mod_module_files,
+    clippy::unwrap_used,
+    //missing_docs,
+    //missing_copy_implementations,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_qualifications
+)]
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
