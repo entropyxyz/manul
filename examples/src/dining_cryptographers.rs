aac21e2a35b7b112d1437666830bd4f7215c233d
David Palm <dvdplm@gmail.com>
2025-05-15 17:24:19 +0200

    Finish draft impl

--- examples/src/dining_cryptographers.rs
+++ examples/src/dining_cryptographers.rs
@@ -38,7 +38,7 @@
 //! Round 1:
 //! 	Each diner DMs their neighbour their cointoss.
 //!     Outcome: the state of each diner is their own cointoss and the bit they received as a DM, i.e. two bits, one
-//!     private and the other shared with their neighbour diner.
+//!     they sent to one neighbour and the other received by their other neighbour diner.
 //! Round 2:
 //! 	Each diner broadcasts one bit: the XOR (if they didn't pay) or ¬XOR (if they paid) of their two bits.
 //! 	Outcome: each diner has a set of bits, one for each diner.
@@ -55,8 +55,8 @@ use manul::{
     // TODO: this is lot of imports, perhaps we should have a "prelude" and have users do `use manul::prelude::*`?
     protocol::{
         Artifact, BoxedFormat, BoxedRound, CommunicationInfo, DirectMessage, EchoBroadcast, EchoRoundParticipation,
-        EntryPoint, FinalizeOutcome, LocalError, MessageValidationError, NormalBroadcast, PartyId, Payload, Protocol,
-        ProtocolError, ProtocolMessage, ProtocolMessagePart, ProtocolValidationError, ReceiveError,
+        EntryPoint, FinalizeOutcome, LocalError, MessageValidationError, NoProtocolErrors, NormalBroadcast, PartyId,
+        Payload, Protocol, ProtocolError, ProtocolMessage, ProtocolMessagePart, ProtocolValidationError, ReceiveError,
         RequiredMessageParts, RequiredMessages, Round, RoundId, TransitionInfo,
     },
     session::SessionParameters,
@@ -68,48 +68,13 @@ use tracing::{debug, info, trace};
 
 #[derive(Debug)]
 pub struct DiningCryptographersProtocol;
-#[derive(displaydoc::Display, Debug, Clone, Serialize, Deserialize)]
-/// bla
-pub enum DiningError {
-    /// bla
-    R1Err,
-    /// bla
-    R2Err,
-}
-// TODO: for protocols that do not have "provable errors" we should have a shortcut and/or blanket impls.
-impl<Id> ProtocolError<Id> for DiningError {
-    type AssociatedData = ();
-    fn required_messages(&self) -> RequiredMessages {
-        match self {
-            DiningError::R1Err => RequiredMessages::new(RequiredMessageParts::direct_message(), None, None),
-            DiningError::R2Err => RequiredMessages::new(RequiredMessageParts::direct_message(), None, None),
-        }
-    }
-
-    fn verify_messages_constitute_error(
-        &self,
-        format: &BoxedFormat,
-        guilty_party: &Id,
-        shared_randomness: &[u8],
-        associated_data: &Self::AssociatedData,
-        message: ProtocolMessage,
-        previous_messages: BTreeMap<RoundId, ProtocolMessage>,
-        combined_echos: BTreeMap<RoundId, BTreeMap<Id, EchoBroadcast>>,
-    ) -> Result<(), ProtocolValidationError> {
-        match self {
-            DiningError::R1Err => Ok(()),
-            DiningError::R2Err => Ok(()),
-        }
-    }
-}
 
 impl<Id> Protocol<Id> for DiningCryptographersProtocol {
     // XOR/¬XOR of the two bits of the diners (one is their own cointoss, the other shared with their neighbour).
     type Result = (bool, bool, bool);
 
-    type ProtocolError = DiningError;
+    type ProtocolError = NoProtocolErrors;
 
-    // TODO: Having blanket impls for these methods would cut down on the boilerplate a bit.
     fn verify_direct_message_is_invalid(
         format: &BoxedFormat,
         round_id: &RoundId,
@@ -137,32 +102,42 @@ impl<Id> Protocol<Id> for DiningCryptographersProtocol {
 
 #[derive(Debug, Clone, Serialize)]
 pub struct Round1 {
-    diner_id: Verifier,
+    diner_id: DinerId,
+    own_toss: bool,
+    paid: bool,
 }
 
 #[derive(Debug, Clone, Serialize)]
 pub struct Round2 {
-    diner_id: Verifier,
+    diner_id: DinerId,
+    own_toss: bool,
+    neighbour_toss: bool,
+    paid: bool,
 }
 
-impl Round<Verifier> for Round1 {
+impl Round<DinerId> for Round1 {
     type Protocol = DiningCryptographersProtocol;
 
+    // Used to define the possible paths to and from this round. This protocol is very simple, it's simply Round 1 ->
+    // Round 2, so we can use the "linear" utility method to set this up.
     fn transition_info(&self) -> TransitionInfo {
         TransitionInfo::new_linear(1.into())
     }
 
-    fn communication_info(&self) -> CommunicationInfo<Verifier> {
+    // Where are we sending messages, and what messages are we expecting to receive?
+    // For this protocol we will send one message to the neighbour diner, and expect to receive one message from the
+    // other neighbour diner.
+    fn communication_info(&self) -> CommunicationInfo<DinerId> {
         let mut message_destinations = BTreeSet::new();
         let mut expecting_messages_from = BTreeSet::new();
         // diner 0 sends a message to diner 1 (0+1 mod 3 = 1)
         // diner 1 sends a message to diner 2 (1+1 mod 3 = 2)
         // diner 2 sends a message to diner 0 (2+1 mod 3 = 0)
-        message_destinations.insert(Verifier((self.diner_id.0 + 1) % 3));
+        message_destinations.insert(DinerId((self.diner_id.0 + 1) % 3));
         // diner 0 expects a bit from diner 2 (0+2 mod 3 = 2)
         // diner 1 expects a bit from diner 0 (1+2 mod 3 = 0)
         // diner 2 expects a bit from diner 1 (2+2 mod 3 = 1)
-        expecting_messages_from.insert(Verifier((self.diner_id.0 + 2) % 3));
+        expecting_messages_from.insert(DinerId((self.diner_id.0 + 2) % 3));
         CommunicationInfo {
             message_destinations,
             expecting_messages_from,
@@ -171,65 +146,88 @@ impl Round<Verifier> for Round1 {
         }
     }
 
+    // This is called when this diner prepares to share a random bit with their neighbour.
     fn make_direct_message(
         &self,
         rng: &mut dyn CryptoRngCore,
         format: &BoxedFormat,
-        destination: &Verifier,
+        destination: &DinerId,
     ) -> Result<(DirectMessage, Option<Artifact>), LocalError> {
-        // This is called when this diner prepares to send a bit to their neighbour.
         info!(
             "[Round1, make_direct_message] from {:?} to {destination:?}",
             self.diner_id
         );
-        let msg = Round1Message {
-            cointoss: rng.next_u64() % 2 == 0,
-        };
+        let msg = Round1Message { toss: self.own_toss };
         let dm = DirectMessage::new(format, msg)?;
-        // TODO: do I need to propagate the state in an Artifact here perhaps?
+
         Ok((dm, None))
     }
 
+    // This is called when this diner receives a bit from their neighbour.
     fn receive_message(
         &self,
         format: &BoxedFormat,
-        from: &Verifier,
+        from: &DinerId,
         message: ProtocolMessage,
-    ) -> Result<Payload, ReceiveError<Verifier, Self::Protocol>> {
-        // This is called when this diner receives a bit from their neighbour.
-        debug!("[Round1, receive_message] from {from:?} to {:?}", self.diner_id);
-        // trace!("[Round1, receive_message] message: {:?}", message);
+    ) -> Result<Payload, ReceiveError<DinerId, Self::Protocol>> {
         let dm = message.direct_message.deserialize::<Round1Message>(format)?;
-        trace!("[Round1, receive_message] message (deserialized dm): {:?}", dm);
-        Ok(Payload::empty())
+        debug!(
+            "[Round1, receive_message] {:?} was dm'd by {from:?}: {dm:?}",
+            self.diner_id
+        );
+        let payload = Payload::new(dm.toss);
+        Ok(payload)
     }
 
     fn finalize(
         self: Box<Self>,
         rng: &mut dyn CryptoRngCore,
-        payloads: BTreeMap<Verifier, Payload>,
-        artifacts: BTreeMap<Verifier, Artifact>,
-    ) -> Result<FinalizeOutcome<Verifier, Self::Protocol>, LocalError> {
+        payloads: BTreeMap<DinerId, Payload>,
+        artifacts: BTreeMap<DinerId, Artifact>,
+    ) -> Result<FinalizeOutcome<DinerId, Self::Protocol>, LocalError> {
+        trace!(
+            "[Round1, finalize] {:?}, payloads len: {}, artifacts len: {}",
+            self.diner_id,
+            payloads.len(),
+            artifacts.len()
+        );
+        let artifacts_d = downcast_artifacts::<bool>(artifacts)?;
+        let payloads_d = downcast_payloads::<bool>(payloads)?;
+        debug!(
+            "[Round1, finalize] {:?} has access to: \n\tpayloads: {payloads_d:?}\n\tartifacts: {artifacts_d:?}",
+            self.diner_id
+        );
+        let neighbour_toss = *payloads_d
+            .first_key_value()
+            .ok_or_else(|| return LocalError::new("No payloads found"))?
+            .1;
+
+        info!(
+            "[Round1, finalize] {:?} is finalizing to Round 2. Own cointoss: {}, neighbour cointoss: {neighbour_toss}",
+            self.diner_id, self.own_toss
+        );
         Ok(FinalizeOutcome::AnotherRound(BoxedRound::new_dynamic(Round2 {
             diner_id: self.diner_id,
+            own_toss: self.own_toss,
+            neighbour_toss,
+            paid: self.paid,
         })))
     }
 }
 
-impl Round<Verifier> for Round2 {
+impl Round<DinerId> for Round2 {
     type Protocol = DiningCryptographersProtocol;
 
     fn transition_info(&self) -> TransitionInfo {
         TransitionInfo::new_linear_terminating(2.into())
     }
 
-    fn communication_info(&self) -> CommunicationInfo<Verifier> {
-        // This is kind of noisy, maybe we can have a utility method for this.
+    fn communication_info(&self) -> CommunicationInfo<DinerId> {
         let everyone_else = [0, 1, 2]
             .iter()
             .filter_map(|id| {
                 if id != &self.diner_id.0 {
-                    Some(Verifier(*id))
+                    Some(DinerId(*id))
                 } else {
                     None
                 }
@@ -248,67 +246,72 @@ impl Round<Verifier> for Round2 {
         #[allow(unused_variables)] rng: &mut dyn CryptoRngCore,
         #[allow(unused_variables)] format: &BoxedFormat,
     ) -> Result<NormalBroadcast, LocalError> {
-        // This is called for this diner to broadcast their secret bit XORed with their shared bit.
         debug!(
             "[Round2, make_normal_broadcast] {:?} broadcasts to everyone else",
             self.diner_id
         );
-        // TODO: this is where the diner should broadcast their XORed bit (or it's negation if they paid).
-        let msg = Round2Message {
-            xor: false, // 0
+        // This diner broadcasts their XORed bit (or it's negation if they paid).
+        let reveal = if self.paid {
+            !self.own_toss ^ self.neighbour_toss
+        } else {
+            self.own_toss ^ self.neighbour_toss
         };
+        let msg = Round2Message { reveal };
         let bcast = NormalBroadcast::new(format, msg)?;
         Ok(bcast)
     }
 
-    // This is called from `Session::process_message` as part of the message delivery loop. If we get here, it means that message preprocessing was successful.
+    // This is called from `Session::process_message` as part of the message delivery loop. If we get here, it means
+    // that message pre-processing was successful.
     fn receive_message(
         &self,
         format: &BoxedFormat,
-        from: &Verifier,
+        from: &DinerId,
         message: ProtocolMessage,
-    ) -> Result<Payload, ReceiveError<Verifier, Self::Protocol>> {
+    ) -> Result<Payload, ReceiveError<DinerId, Self::Protocol>> {
         debug!("[Round2, receive_message] from {from:?} to {:?}", self.diner_id);
-        trace!("[Round2, receive_message] message: {:?}", message); //TODO: this is empty, need to impl something to ensure Round 2 broadcast is sent.
         let bcast = message.normal_broadcast.deserialize::<Round2Message>(format)?;
         trace!("[Round2, receive_message] message (deserialized bcast): {:?}", bcast);
-        // TODO: What should go into the Payload? Is this "kept" and propagated to the final outcome?
-        let payload = Payload::new(bcast.xor);
+        // The payload is kept and delivered in the `finalize` method.
+        let payload = Payload::new(bcast.reveal);
         Ok(payload)
     }
 
     fn finalize(
         self: Box<Self>,
         rng: &mut dyn CryptoRngCore,
-        payloads: BTreeMap<Verifier, Payload>,
-        artifacts: BTreeMap<Verifier, Artifact>,
-    ) -> Result<FinalizeOutcome<Verifier, Self::Protocol>, LocalError> {
+        payloads: BTreeMap<DinerId, Payload>,
+        artifacts: BTreeMap<DinerId, Artifact>,
+    ) -> Result<FinalizeOutcome<DinerId, Self::Protocol>, LocalError> {
+        let artifacts_d = downcast_artifacts::<bool>(artifacts)?;
+        let payloads_d = downcast_payloads::<bool>(payloads)?;
         debug!(
-            "[Round2, finalize] {:?}\n\tpayloads: {payloads:?}\n\tartifacts: {artifacts:?}",
+            "[Round2, finalize] {:?}\n\tpayloads: {payloads_d:?}\n\tartifacts: {artifacts_d:?}",
             self.diner_id
         );
-
-        let mut bits = payloads
-            .into_values()
-            .map(|p| p.downcast())
-            .collect::<Result<Vec<bool>, _>>()?;
-        // TODO: How do I access the bit of this diner?, i.e. my own state?
-        let my_bit = true;
-        bits.push(my_bit);
-        assert!(bits.len() == 3, "Expected 3 diners and 3 bits, instead got {bits:?}");
-        Ok(FinalizeOutcome::Result((bits[0], bits[1], bits[2])))
+        let bits = payloads_d.values().cloned().collect::<Vec<_>>();
+        let mut own_reveal = self.own_toss ^ self.neighbour_toss;
+        if self.paid {
+            own_reveal = !own_reveal;
+        }
+        assert!(
+            bits.len() + 1 == 3,
+            "Expected 3 diners and 3 bits, instead got {bits:?}"
+        );
+        Ok(FinalizeOutcome::Result((bits[0], bits[1], own_reveal)))
     }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Round1Message {
-    pub cointoss: bool,
+    // The cointoss of this diner, which is shared with their neighbour.
+    toss: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Round2Message {
-    // TODO: better name for this, maybe `reveal`?
-    pub xor: bool,
+    // The result of XORing the two bits: own cointoss and that of their neighbour.
+    reveal: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -322,43 +325,55 @@ impl DiningEntryPoint {
     }
 }
 
-impl EntryPoint<Verifier> for DiningEntryPoint {
+impl EntryPoint<DinerId> for DiningEntryPoint {
     type Protocol = DiningCryptographersProtocol;
 
     fn entry_round_id() -> RoundId {
+        // We start at Round 1
         1.into()
     }
 
     // Called as part of the session initialization, specifically in [`Session::new`].
     // Each `EntryPoint` creates one `Session`.
+    // TODO: Can this ever NOT create the first round? Would a rename to "make_first_round" be appropriate?
     fn make_round(
         self,
-        _rng: &mut dyn CryptoRngCore,
+        rng: &mut dyn CryptoRngCore,
         _shared_randomness: &[u8],
-        id: &Verifier,
-    ) -> Result<BoxedRound<Verifier, Self::Protocol>, LocalError> {
-        let round = Round1 { diner_id: id.clone() };
+        id: &DinerId,
+    ) -> Result<BoxedRound<DinerId, Self::Protocol>, LocalError> {
+        let paid = id.0 == 0 && rng.next_u32() % 2 == 0;
+        let round = Round1 {
+            diner_id: id.clone(),
+            own_toss: rng.next_u32() % 2 == 0,
+            paid,
+        };
+        trace!(
+            "[DiningEntryPoint, make_round] diner {id:?} tossed: {:?} (paid? {paid})",
+            round.own_toss
+        );
         let round = BoxedRound::new_dynamic(round);
         Ok(round)
     }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize, Ord, PartialOrd, Eq, PartialEq)]
-// TODO Need to store the id of the diner and whether they paid or not.
+// TODO: this type is not used at all. It'd be the natural place for things like "paid for dinner" and other similar
+// state, but I don't see how to access a `Diner` from inside the protocol, without doing nasty stuff like having a
+// global "state" thingy (standing in for a database).
 pub struct Diner {
     id: u8,
-    paid: bool,
 }
 impl Diner {
-    fn new(id: u8, paid: bool) -> Self {
-        Self { id, paid }
+    fn new(id: u8) -> Self {
+        Self { id }
     }
 }
 
 // TODO: I don't understand why there needs to be a Signer and a Verifier type.
 // Suspect it's a CGGMP "contamination" and that they are not needed at all for this protocol.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
-pub struct Verifier(u8);
+pub struct DinerId(u8);
 
 // TODO: same here, why do we need a signature type?
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
@@ -382,14 +397,14 @@ impl<D: digest::Digest> signature::RandomizedDigestSigner<D, DinerSignature> for
 }
 // TODO: this feels like boilerplate.
 impl signature::Keypair for Diner {
-    type VerifyingKey = Verifier;
+    type VerifyingKey = DinerId;
 
     fn verifying_key(&self) -> Self::VerifyingKey {
-        Verifier(self.id)
+        DinerId(self.id)
     }
 }
 // TODO: this feels like boilerplate.
-impl<D: digest::Digest> signature::DigestVerifier<D, DinerSignature> for Verifier {
+impl<D: digest::Digest> signature::DigestVerifier<D, DinerSignature> for DinerId {
     fn verify_digest(&self, _digest: D, signature: &DinerSignature) -> Result<(), signature::Error> {
         if self.0 == signature.signed_by {
             Ok(())
@@ -403,16 +418,28 @@ pub struct DiningSessionParams;
 
 impl SessionParameters for DiningSessionParams {
     type Signer = Diner;
-    type Verifier = Verifier;
+    type Verifier = DinerId;
     type Signature = DinerSignature;
     type Digest = TestHasher;
     type WireFormat = BinaryFormat;
 }
 
+// TODO: would be nice to have `downcast_all` from `synedrion`.
+fn downcast_payloads<T: 'static>(map: BTreeMap<DinerId, Payload>) -> Result<BTreeMap<DinerId, T>, LocalError> {
+    map.into_iter()
+        .map(|(id, payload)| payload.downcast::<T>().map(|p| (id, p)))
+        .collect()
+}
+fn downcast_artifacts<T: 'static>(map: BTreeMap<DinerId, Artifact>) -> Result<BTreeMap<DinerId, T>, LocalError> {
+    map.into_iter()
+        .map(|(id, artifact)| artifact.downcast::<T>().map(|p| (id, p)))
+        .collect()
+}
+
 fn main() {
     tracing_subscriber::fmt::init();
     info!("Dining Cryptographers Protocol Example");
-    let diners = (0..=2).map(|id| Diner::new(id, false)).collect::<Vec<_>>();
+    let diners = (0..=2).map(|id| Diner::new(id)).collect::<Vec<_>>();
 
     let all_diners = diners
         .iter()
@@ -430,8 +457,16 @@ fn main() {
         .results()
         .unwrap();
 
-    // `results` should contain 3 booleans, which, when XORed together, should be 0 if the NSA paid, or 1 if one of the diners paid.
-    for (_id, result) in results {
-        info!("Result: {:?}", result);
+    // `results` contains 3 booleans, which, when XORed together, make `true` if one of the diners paid, or `false` if the NSA paid.
+    for (id, result) in results {
+        let who_paid = if result.0 ^ result.1 ^ result.2 {
+            "one of the diners"
+        } else {
+            "the NSA"
+        };
+        info!(
+            "Diner {id:?} got result: {:?}; they conclude that {who_paid} paid for dinner!",
+            result
+        );
     }
 }
